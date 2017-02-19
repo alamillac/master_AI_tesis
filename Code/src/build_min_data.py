c9c20623e1726c66d98043b71e6e2dfeccc376d3
Andres <andres@lamillac.com>
2017-02-19 20:10:11 +0100

    Graficas de estadísticas distribución de los datos

--- Code/src/build_min_data.py
+++ Code/src/build_min_data.py
@@ -3,6 +3,10 @@
 from os import path
 from datasetGenerator import DatasetGenerator
 import logging
+import matplotlib.pyplot as plt
+import numpy as np
+
+import sys
 
 logging.basicConfig()
 
@@ -22,21 +26,38 @@ logger.debug("Generating dataset 100%")
 ratings_100 = generator.getDataset(percentage=1)
 logger.debug("Stats from dataset 100%")
 stats_100 = generator.getStatsFromDataset(ratings_100)
-logger.debug(stats_100)
+
+#plt.hist(stats_100['countRatingsByUsers'])
 logger.debug("Generating dataset 55%")
 ratings_55 = generator.getDataset(percentage=0.55)
+
 logger.debug("Generating dataset 30%")
 ratings_30 = generator.getDataset(percentage=0.3)
+stats_30 = generator.getStatsFromDataset(ratings_30)
 
-# get optimus dataset 60%
-logger.debug("Generating optimus dataset 60%")
-ratings = generator.getOptimusDatasetPercentage(percentage=0.6)
+# get optimus dataset 40%
+logger.debug("Generating optimus dataset 40%")
+ratings_opt_40 = generator.getOptimusDatasetPercentage(percentage=0.4)
+logger.debug("Stats from dataset optimus 40%")
+stats_opt_40 = generator.getStatsFromDataset(ratings_opt_40)
 
 # get optimus dataset
 num_users = 1000
 num_movies = 2000
 logger.debug("Generating optimus dataset users=%d, movies=%d" % (num_users, num_movies))
 ratings_opt = generator.getOptimusDataset(best_users=num_users, best_movies=num_movies)
+logger.debug("Stats from dataset optimus")
+stats_opt = generator.getStatsFromDataset(ratings_opt)
+
+bins = np.linspace(0, 2500, 100)
+plt.hist(stats_100['countRatingsByUsers'], bins, normed=1, alpha=0.5)
+plt.hist(stats_30['countRatingsByUsers'], bins, normed=1, alpha=0.5)
+plt.hist(stats_opt_40['countRatingsByUsers'], bins, normed=1, alpha=0.5)
+plt.hist(stats_opt['countRatingsByUsers'], bins, normed=1, alpha=0.5)
+plt.title("Histogram user ratings")
+plt.show()
+
+sys.exit(0)
 
 # Save it
 rating_min_filename = path.join(DATA_DIR, 'ratings_min.csv')
